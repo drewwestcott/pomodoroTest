1e49e1cb81ec36386e3022efec64834bd2b6f212
Mr Drew Westcott <drew.westcott@me.com>
2016-07-04 18:14:02 +0100

    Initial Commit

---
+++ Pomodoro-From-Tasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pomodoro-From-Tasks
+//
+//  Created by Drew Westcott on 04/07/2016.
+//  Copyright © 2016 Drew Westcott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
